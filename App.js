a17d6312270a3ebc4c7e5514a8684905c6e7bb20
mysilkyheart <deanheart09@gmail.com>
2018-12-18 07:49:19 +0700

    this

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import React, { Component } from 'react'
+import { addNavigationHelpers } from 'react-navigation'
+import { createReduxBoundAddListener } from 'react-navigation-redux-helpers'
+import { Provider, connect } from 'react-redux'
+import RootNavigator from './app/navigator/RootNavigator'
+import store from './app/store'
 
-import { Login } from './app/screen'
+const addListener = createReduxBoundAddListener("root");
 
-const App = createStackNavigator({
-    Login: {
-        screen: Login
+class App extends Component {
+    render() {
+        return (
+            <RootNavigator navigation={addNavigationHelpers({
+                dispatch: this.props.dispatch,
+                state: this.props.nav,
+                addListener
+            })} />
+        )
     }
+}
+
+const mapStateToProps = (state) => ({
+    nav: state.nav
 })
 
-export default createAppContainer(App)
+const AppWithNavigationState = connect(mapStateToProps)(App)
+
+export default class Root extends Component {
+    render() {
+        <Provider store={store}>
+            <AppWithNavigationState />
+        </Provider>
+    }
+}
--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-import React, { Component } from 'react'
-import { AppRegistry } from 'react-native'
-import App from './App'
-import { name as appName } from './app.json'
-import { Provider } from 'react-redux'
-import configureStore from './app/store'
+import { AppRegistry } from 'react-native';
+import {name as appName} from './app.json';
+import App from './App';
 
-class Index extends Component {
-    render() {
-        return (
-            <Provider store={configureStore}>
-                <App />
-            </Provider>
-        )
-    }
-}
-
-AppRegistry.registerComponent(appName, () => Index)
+AppRegistry.registerComponent(appName, () => App);
