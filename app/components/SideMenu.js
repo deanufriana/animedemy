4df3f269714ba8e2737d41c2a51b1af589f1a205
prasetya12 <rizalprasetya22@gmail.com>
2018-12-21 17:33:12 +0700

    add features video

--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -1,90 +1,126 @@
-import { Container, Button, ListItem, List, Icon } from 'native-base'
-import React, { Component } from 'react'
-import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient';
-import deviceStorage from '../data/deviceStorage'
+import { Container, Button, ListItem, List, Icon } from "native-base";
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableWithoutFeedback
+} from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import deviceStorage from "../data/deviceStorage";
+
+import { NavigationActions } from "react-navigation";
 
 class SideMenu extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isSubsribe: false,
-      isLogin: false,
-    }
+      isLogin: false
+    };
   }
 
   displayButton() {
     if (deviceStorage.getKey()) {
       if (!this.state.isSubsribe) {
         return (
-          <Button style={{ width: 100, backgroundColor: '#CE0606', alignSelf: 'center', justifyContent: 'center', alignItems: 'center', height: 40 }} onPress={() => this.setState({ isSubsribe: this.state.isSubsribe })}>
-            <Text style={{ color: 'white' }} >
-              Subscribe
-            </Text>
+          <Button
+            style={{
+              width: 100,
+              backgroundColor: "#CE0606",
+              alignSelf: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              height: 40
+            }}
+            onPress={() => this.setState({ isSubsribe: this.state.isSubsribe })}
+          >
+            <Text style={{ color: "white" }}>Subscribe</Text>
           </Button>
-        )
+        );
       } else {
         return (
-          <Button style={{ width: 100, backgroundColor: '#010101', borderColor: '#CE0606', alignSelf: 'center', borderWidth: 2, justifyContent: 'center', alignItems: 'center', height: 40 }} onPress={() => this.setState({ isSubsribe: false })}>
-            <Text style={{ color: '#CE0606' }}>
-              Unsubscribe
-            </Text>
+          <Button
+            style={{
+              width: 100,
+              backgroundColor: "#010101",
+              borderColor: "#CE0606",
+              alignSelf: "center",
+              borderWidth: 2,
+              justifyContent: "center",
+              alignItems: "center",
+              height: 40
+            }}
+            onPress={() => this.setState({ isSubsribe: false })}
+          >
+            <Text style={{ color: "#CE0606" }}>Unsubscribe</Text>
           </Button>
-        )
+        );
       }
     } else {
       return (
-        <Button style={{ backgroundColor: '#00C0C1', alignSelf: 'center', width: 100, height: 40, justifyContent: 'center' }} onPress={() => this.props.navigation.navigate('Login')}>
-          <Text style={{ color: 'white' }} >
-            Login
-          </Text>
+        <Button
+          style={{
+            backgroundColor: "#00C0C1",
+            alignSelf: "center",
+            width: 100,
+            height: 40,
+            justifyContent: "center"
+          }}
+          onPress={() => this.props.navigation.navigate("Login")}
+        >
+          <Text style={{ color: "white" }}>Login</Text>
         </Button>
-      )
-
+      );
     }
   }
 
   logout = () => {
-    if (deviceStorage.removeKey('id_token')) {
-      return this.props.navigation.navigate('Login')
+    if (deviceStorage.removeKey("id_token")) {
+      return this.props.navigation.navigate("Login");
     }
-    return alert('Terjadi Kesalahan')
-  }
+    return alert("Terjadi Kesalahan");
+  };
 
   displayLogout() {
     if (deviceStorage.getKey()) {
       return (
-        <Button style={{ width: '100%', backgroundColor: 'red', height: 40 }} onPress={this.logout} >
-          <Text style={{ color: 'white', marginLeft: 20 }}>
-            Logout
-            </Text>
+        <Button
+          style={{ width: "100%", backgroundColor: "red", height: 40 }}
+          onPress={this.logout}
+        >
+          <Text style={{ color: "white", marginLeft: 20 }}>Logout</Text>
         </Button>
-      )
+      );
     } else {
-      return (
-        <View style={{ height: 40 }}>
-
-        </View>
-      )
+      return <View style={{ height: 40 }} />;
     }
   }
 
-
   render() {
-    { alert(JSON.stringify(deviceStorage.getKey())) }
+    // {
+    //   alert(JSON.stringify(deviceStorage.getKey()));
+    // }
     return (
-
       <Container>
-        <LinearGradient colors={['#010101', '#1A222E']} style={styles.linearGradient}>
-          <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
+        <LinearGradient
+          colors={["#010101", "#1A222E"]}
+          style={styles.linearGradient}
+        >
+          <View
+            style={{ flex: 3, alignItems: "center", justifyContent: "center" }}
+          >
             <View style={{ flex: 1, marginTop: 10 }}>
-              <Image source={require('../assets/img/logo.png')} style={{ width: 140, height: 50 }} />
+              <Image
+                source={require("../assets/img/logo.png")}
+                style={{ width: 140, height: 50 }}
+              />
             </View>
             <View style={{ flex: 2, marginTop: 20 }}>
               {this.displayButton()}
             </View>
-
-
 
             {/* {this.state.isSubsribe?(<Button style={{padding:10,backgroundColor:'#CE0606',alignSelf:'center'}} onPress={()=>this.setState({isSubsribe:false})}>
               <Text style={{color:'white'}} >
@@ -97,51 +133,79 @@ class SideMenu extends Component {
               </Text>
             </Button>
             )} */}
-
           </View>
-          <View style={{ flex: 2, marginLeft: 20, justifyContent: 'center' }} >
-            <Text style={{ color: 'white', fontFamily: 'Roboto-Medium', fontSize: 14 }}>
-              Directory
-            </Text>
-          </View>
+          <TouchableWithoutFeedback  onPress={()=> this.props.navigation.navigate('Directory')}>
+            <View style={{ flex: 2, marginLeft: 20, justifyContent: "center" }} >
+              <Text
+                style={{
+                  color: "white",
+                  fontFamily: "Roboto-Medium",
+                  fontSize: 14
+                }}
+              >
+                Directory
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
 
-          <View style={{ flex: 9, flexDirection: 'column' }}>
-
-            <Text style={{ fontSize: 24, color: '#00C0C1', fontFamily: 'Roboto-Medium', marginLeft: 10 }}>
+          <View style={{ flex: 9, flexDirection: "column" }}>
+            <Text
+              style={{
+                fontSize: 24,
+                color: "#00C0C1",
+                fontFamily: "Roboto-Medium",
+                marginLeft: 10
+              }}
+            >
               Category
             </Text>
 
-            <View style={{ borderTopColor: '#00C0C1', borderTopWidth: 3, marginTop: 5 }}></View>
+            <View
+              style={{
+                borderTopColor: "#00C0C1",
+                borderTopWidth: 3,
+                marginTop: 5
+              }}
+            />
             <List>
-              <ListItem onPress={() => this.props.navigation.navigate('Category')}>
-                <Text style={{ color: 'white', fontFamily: 'Roboto-Medium', fontSize: 14 }}>
+              <ListItem
+                onPress={() => this.props.navigation.navigate('Category')}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontFamily: "Roboto-Medium",
+                    fontSize: 14
+                  }}
+                >
                   Action
-                  </Text>
+                </Text>
               </ListItem>
               <ListItem>
-                <Text style={{ color: 'white', fontFamily: 'Roboto-Medium', fontSize: 14 }}>
+                <Text
+                  style={{
+                    color: "white",
+                    fontFamily: "Roboto-Medium",
+                    fontSize: 14
+                  }}
+                >
                   Romance
-                  </Text>
+                </Text>
               </ListItem>
-
             </List>
-
-
-
           </View>
 
           {this.displayLogout()}
         </LinearGradient>
       </Container>
-    )
+    );
   }
 }
 
-export default SideMenu
+export default SideMenu;
 
 const styles = StyleSheet.create({
   linearGradient: {
-    flex: 1,
-
+    flex: 1
   }
-})
+});
--- app/screens/MovieScreen.js
+++ app/screens/MovieScreen.js
@@ -1,163 +1,293 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, Dimensions, ImageBackground, TouchableOpacity } from 'react-native'
-import { Container, Content, Icon } from 'native-base';
-import LinearGradient from 'react-native-linear-gradient'
-import Star from 'react-native-star-view';
-import TryTab from '../components/TryTab'
-import Video from 'react-native-video'
-import Orientation from 'react-native-orientation'
-import { DETAIL_VIDEO } from '../actions/video';
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  TouchableOpacity, Platform
+} from "react-native";
+import { Container, Content, Icon } from "native-base";
+import LinearGradient from "react-native-linear-gradient";
+import Star from "react-native-star-view";
+import TryTab from "../components/TryTab";
+import Video from "react-native-video";
+import Orientation from "react-native-orientation";
+import { DETAIL_VIDEO } from "../actions/video";
+import { connect } from "react-redux";
+
+import WebView from 'react-native-android-fullscreen-webview-video';
 
 class MovieScreen extends Component {
+  uri =
+    "https://www.blogger.com/video.g?token=AD6v5dzsk56t_uLeu8AeKuSIOzbe_vn56qlUxwtFA9hQl8MxFiu9bZ9yyVWG0zfJKF9CTISPsfsDhIdBx1lejenfBANnF6s_4fxLe5VKN4DjpMJYRH6DOWDfGrmY8uf9aNxeUeX5RH0m";
 
-    componentDidMount() {
-        this.props.dispatch(DETAIL_VIDEO(this.props.navigation.getParam('id')))
-        Orientation.lockToPortrait()
+  componentDidMount() {
+    this.props.dispatch(DETAIL_VIDEO(this.props.navigation.getParam("id")));
+    Orientation.lockToPortrait();
+  }
+
+  state = {
+    pause: true,
+    fullscreen: false
+  };
+
+  fullscreen = () => {
+    this.setState({ fullscreen: !this.state.fullscreen });
+    if (!this.state.fullscreen) {
+      Orientation.lockToLandscape();
+    } else {
+      Orientation.lockToPortrait();
     }
+  };
 
-    state = {
-        pause: true,
-        fullscreen: false
+  press = () => this.setState({ pause: !this.state.pause });
+
+  goBack = () => {
+    if (this.state.fullscreen) {
+      Orientation.lockToPortrait();
+      this.setState({ fullscreen: !this.state.fullscreen });
+    } else {
+      this.props.navigation.goBack();
     }
+  };
 
-    fullscreen = () => {
-        this.setState({ fullscreen: !this.state.fullscreen })
-        if (!this.state.fullscreen) {
-            Orientation.lockToLandscape()
-        } else {
-            Orientation.lockToPortrait()
-        }
-    }
-
-    press = () => this.setState({ pause: !this.state.pause })
-
-    goBack = () => {
-        if (this.state.fullscreen) {
-            Orientation.lockToPortrait()
-            this.setState({ fullscreen: !this.state.fullscreen })
-        } else {
-            this.props.navigation.goBack()
-        }
-    }
-
-    controlButton = () => {
-        if (this.state.pause) {
-            return (
-                <ImageBackground source={{ uri: this.props.detail.data[0].image_url }} style={styles.image}>
-                    <View style={{ flex: 1 }}>
-                        <TouchableOpacity onPress={this.goBack}>
-                            <View style={{ alignSelf: 'flex-start', marginLeft: 10, marginTop: 10, borderRadius: 150 / 2, backgroundColor: 'white', height: 30, width: 30, alignItems: 'center', justifyContent: 'center' }}>
-                                <Icon name='md-arrow-back' style={{ color: '#00C0C1' }} />
-                            </View>
-                        </TouchableOpacity>
-                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-                            <Icon name='skip-backward' style={{ color: 'white', marginHorizontal: 50 }} />
-
-                            <Icon onPress={this.press} name='play' style={{ color: 'white', marginHorizontal: 50 }} />
-
-                            <Icon name='skip-forward' style={{ color: 'white', marginHorizontal: 50 }} />
-                        </View>
-                        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-                            <LinearGradient colors={['rgba(0,0,0,0)', '#1A222E']} style={{ height: 80, width: '100%' }}>
-                                <View style={{ justifyContent: 'center', flex: 1, marginLeft: 16 }}>
-                                    <Text style={{ fontSize: 18, color: 'white', fontFamily: 'OpenSans-Bold' }}>{this.props.detail.data[0].title}</Text>
-                                </View>
-                                <View style={{ flexDirection: 'row', flex: 1, marginLeft: 16 }}>
-                                    <View style={{ flex: 1, marginTop: 3, borderRightColor: 'white', borderRightWidth: 3, height: 20 }}>
-                                        <Text style={{ fontFamily: 'Open-Sans-Bold', fontSize: 14, color: 'white' }}>
-                                            {this.props.detail.data[0].age_restriction}
-                                        </Text>
-                                    </View>
-
-                                    <View style={{ flexDirection: 'row', flex: 8, marginTop: 1, marginLeft: 10 }}>
-                                        <Star score={this.props.detail.data[0].imdb_score / 2} style={{ width: 90, height: 16, marginTop: 3 }} />
-                                        <Text style={{ fontFamily: 'OpenSans-Bold', fontSize: 16, color: 'white', marginLeft: 5 }}>{this.props.detail.data[0].imdb_score}</Text>
-                                    </View>
-
-                                    <View style={{ alignSelf: 'flex-end', margin: 10, }}>
-                                        <Icon style={{ color: 'white' }} name={this.state.fullscreen ? "phone-portrait" : "phone-landscape"} onPress={this.fullscreen} />
-                                    </View>
-                                </View>
-                            </LinearGradient>
-                        </View>
-                    </View>
-                </ImageBackground>
-            )
-        }
-    }
-
-    video = () => {
-        if (!this.props.detail.isLoading) {
-            return <TouchableOpacity style={{ justifyContent: 'space-between', flex: 1 }} onPress={this.press}>
-                <Video source={{ uri: this.props.detail.data[0].video_url }}   // Can be a URL or a local file.
-                    ref={(ref) => {
-                        this.player = ref
-                    }}
-                    // poster={ "http://cdn56.picsart.com/175939569000202.gif"}
-                    // posterResizeMode={'cover'}                                // Store reference
-                    onBuffer={this.Buffer}                // Callback when remote video is buffering
-                    onError={this.videoError}
-                    fullscreen={this.state.fullscreen}
-                    resizeMode={"stretch"}
-                    paused={this.state.pause}
-                    // Callback when video cannot be loaded
-                    style={styles.backgroundVideo}
-                />
-
-                {this.controlButton()}
+  controlButton = () => {
+    if (this.state.pause) {
+      return (
+        <ImageBackground
+          source={{ uri: this.props.detail.data[0].image_url }}
+          style={styles.image}
+        >
+          <View style={{ flex: 1 }}>
+            <TouchableOpacity onPress={this.goBack}>
+              <View
+                style={{
+                  alignSelf: "flex-start",
+                  marginLeft: 10,
+                  marginTop: 10,
+                  borderRadius: 150 / 2,
+                  backgroundColor: "white",
+                  height: 30,
+                  width: 30,
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+              >
+                <Icon name="md-arrow-back" style={{ color: "#00C0C1" }} />
+              </View>
             </TouchableOpacity>
-        } else {
-            return <Text>Loading</Text>
-        }
-    }
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                flex: 1
+              }}
+            >
+              <Icon
+                name="skip-backward"
+                style={{ color: "white", marginHorizontal: 50 }}
+              />
 
-    render() {
-        console.log(this.props.detail)
-        return (
-            <Container>
-                {this.video()}
-                {
-                    this.state.fullscreen ||
-                    <LinearGradient colors={['#010101', '#1A222E']} style={styles.linearGradient}>
-                        <View style={{ flex: 1, backgroundColor: '#1A222E' }}>
-                            <TryTab />
-                        </View>
-                    </LinearGradient>
-                }
-            </Container>
-        )
+              <Icon
+                onPress={this.press}
+                name="play"
+                style={{ color: "white", marginHorizontal: 50 }}
+              />
+
+              <Icon
+                name="skip-forward"
+                style={{ color: "white", marginHorizontal: 50 }}
+              />
+            </View>
+            <View style={{ flex: 1, justifyContent: "flex-end" }}>
+              <LinearGradient
+                colors={["rgba(0,0,0,0)", "#1A222E"]}
+                style={{ height: 80, width: "100%" }}
+              >
+                <View
+                  style={{ justifyContent: "center", flex: 1, marginLeft: 16 }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      color: "white",
+                      fontFamily: "OpenSans-Bold"
+                    }}
+                  >
+                    {this.props.detail.data[0].title}
+                  </Text>
+                </View>
+                <View style={{ flexDirection: "row", flex: 1, marginLeft: 16 }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      marginTop: 3,
+                      borderRightColor: "white",
+                      borderRightWidth: 3,
+                      height: 20
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontFamily: "Open-Sans-Bold",
+                        fontSize: 14,
+                        color: "white"
+                      }}
+                    >
+                      {this.props.detail.data[0].age_restriction}
+                    </Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      flex: 8,
+                      marginTop: 1,
+                      marginLeft: 10
+                    }}
+                  >
+                    <Star
+                      score={this.props.detail.data[0].imdb_score / 2}
+                      style={{ width: 90, height: 16, marginTop: 3 }}
+                    />
+                    <Text
+                      style={{
+                        fontFamily: "OpenSans-Bold",
+                        fontSize: 16,
+                        color: "white",
+                        marginLeft: 5
+                      }}
+                    >
+                      {this.props.detail.data[0].imdb_score}
+                    </Text>
+                  </View>
+
+                  <View style={{ alignSelf: "flex-end", margin: 10 }}>
+                    <Icon
+                      style={{ color: "white" }}
+                      name={
+                        this.state.fullscreen
+                          ? "phone-portrait"
+                          : "phone-landscape"
+                      }
+                      onPress={this.fullscreen}
+                    />
+                  </View>
+                </View>
+              </LinearGradient>
+            </View>
+          </View>
+        </ImageBackground>
+      );
     }
+  };
+
+  // source={{ uri: this.props.detail.data[0].video_url }}
+
+  video = () => {
+    if (!this.props.detail.isLoading) {
+      return (
+        <TouchableOpacity
+          style={{ justifyContent: "space-between", flex: 1 }}
+          onPress={this.press}
+        >
+          <Video
+            source={{ uri: this.props.detail.data[0].video_url }} // Can be a URL or a local file.
+            ref={ref => {
+              this.player = ref;
+            }}
+            // poster={ "http://cdn56.picsart.com/175939569000202.gif"}
+            // posterResizeMode={'cover'}                                // Store reference
+            onBuffer={this.Buffer} // Callback when remote video is buffering
+            onError={this.videoError}
+            fullscreen={this.state.fullscreen}
+            resizeMode={"stretch"}
+            paused={this.state.pause}
+            // Callback when video cannot be loaded
+            style={styles.backgroundVideo}
+          />
+
+          {this.controlButton()}
+        </TouchableOpacity>
+      );
+    } else {
+      return <Text>Loading</Text>;
+    }
+  };
+
+  render() {
+    console.log(this.props.detail);
+    return (
+      <Container>
+        {/* {this.video()} */}
+
+        {/* <View style={{ height: 300 }}>
+          <WebView
+            style={styles.WebViewContainer}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            source={{ uri: "https://www.blogger.com/video.g?token=AD6v5dzsk56t_uLeu8AeKuSIOzbe_vn56qlUxwtFA9hQl8MxFiu9bZ9yyVWG0zfJKF9CTISPsfsDhIdBx1lejenfBANnF6s_4fxLe5VKN4DjpMJYRH6DOWDfGrmY8uf9aNxeUeX5RH0m" }}
+          />
+        </View> */}
+
+        <View style={{height:200}}>
+        <WebView
+            style={styles.WebViewContainer}
+            source={{ uri: "https://r7---sn-4pgnuhxqp5-jb3s.googlevideo.com/videoplayback?id=3eb6c58f6f3e2d7e&itag=18&source=blogger&mm=31&mn=sn-4pgnuhxqp5-jb3s&ms=au&mv=m&pl=19&ei=TL8cXPuBIozj-gOqs7ioCA&susc=bl&mime=video/mp4&dur=1434.017&lmt=1544958167672166&mt=1545387708&ip=139.192.149.86&ipbits=0&expire=1545416652&sparams=ip,ipbits,expire,id,itag,source,mm,mn,ms,mv,pl,ei,susc,mime,dur,lmt&signature=9187D99259430F52055661AEFEF35EEB83CE5A589DE388E217AEC0264ED2221E.56A32E4693A9226D931E3FAE1590B71F19920EC1850474FFB4346EDC65F4DAA9&key=us0&cpn=ExD69gQQ9RpUGEwF&c=WEB_EMBEDDED_PLAYER&cver=20181220" }}
+          />
+
+        </View>
+
+
+
+        {this.state.fullscreen || (
+          <LinearGradient
+            colors={["#010101", "#1A222E"]}
+            style={styles.linearGradient}
+          >
+            <View style={{ flex: 1, backgroundColor: "#1A222E" }}>
+              <TryTab />
+            </View>
+          </LinearGradient>
+        )}
+      </Container>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-    detail: state.detailReducers
-})
+const mapStateToProps = state => ({
+  detail: state.detailReducers
+});
 
-export default connect(mapStateToProps)(MovieScreen)
+export default connect(mapStateToProps)(MovieScreen);
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        height: 180,
-    },
-    backgroundVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        width: null,
-        height: null
-    },
-    image: {
-        width: null,
-        height: null,
-        flex: 1
-
-    },
-    linearGradient: {
-        flex: 2,
-
-    }
-})
+  container: {
+    justifyContent: "center",
+    height: 180
+  },
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: null,
+    height: null
+  },
+  image: {
+    width: null,
+    height: null,
+    flex: 1
+  },
+  linearGradient: {
+    flex: 2
+  },
+  WebViewContainer: {
+ 
+    marginTop: (Platform.OS == 'ios') ? 20 : 0,
+ 
+  }
+});
